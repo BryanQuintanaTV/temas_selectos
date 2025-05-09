4157a7561d9b4f23861258e9232d1f2695cc6c75
Bryan Quintana <18308051220409@cbtis122.edu.mx>
2025-05-09 08:25:18 -0600

    UpdateV0.3.0d

--- chatbot_back/settings.py
+++ chatbot_back/settings.py
@@ -31,6 +31,22 @@ ALLOWED_HOSTS = [
     "https://apichat.bryanquintana.com"
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "https://chatbot.bryanquintana.com",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
 
 # Application definition
 
@@ -47,6 +63,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,7 +71,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
 
